7fd399674068cfe590c31d63d60f99d40934ccf6
mcpeblocker <mcpeblocker@gmail.com>
2021-10-07 18:50:15 +0500

    edited_message

--- src/config/composer.js
+++ src/config/composer.js
@@ -7,8 +7,8 @@ const manager = new Composer();
 
 manager.use(isGroup);
 
-manager.on('message', async (ctx) => {
-    let message = ctx.message;
+let checkLink = async (ctx) => {
+    let message = ctx.message || ctx.editedMessage;
     let isUrl = hasLink(message.entities || message.caption_entities);
     if (isUrl) {
         let allowed = await isAllowed(ctx);
@@ -19,6 +19,31 @@ manager.on('message', async (ctx) => {
         let text = texts.warning({ name: ctx.from.first_name, id: ctx.from.id });
         ctx.replyWithHTML(text);
     }
+}
+
+manager.on('message', async (ctx) => {
+    await checkLink(ctx);
 });
+
+manager.on('edited_message', async (ctx) => {
+    await checkLink(ctx);
+})
+
+// manager.on('chat_member', (ctx) => {
+//     console.log('member');
+// });
+
+// manager.on('new_chat_members', (ctx) => {
+//     console.log('new');
+//     console.log(ctx.chatMember);
+// });
+
+// manager.on('my_chat_member', (ctx) => {
+//     console.log('my')
+// })
+
+// manager.on('left_chat_member', (ctx) => {
+//     console.log('left');
+// })
 
 module.exports = manager;
--- src/constants/text.js
+++ src/constants/text.js
@@ -2,13 +2,13 @@ const warning = (options) => {
     return `<b>⚠️ Hurmatli, <a href="tg://user?id=${options.id}">${options.name}</a>, guruhda reklama tarqatish mumkin emas!</b>`
 };
 
-const welcome = (options) => {
-    return `👋 Assalomu alaykum, <b>${options.name}.</b> Reklama tozalovchi botga xush kelibsiz. Botdan foydalanish uchun shunchaki uni guruhda admin qiling!`;
-}
+// const welcome = (options) => {
+//     return `👋 Assalomu alaykum, <b>${options.name}.</b> Reklama tozalovchi botga xush kelibsiz. Botdan foydalanish uchun shunchaki uni guruhda admin qiling!`;
+// }
 
 const texts = {
     warning,
-    welcome
+    // welcome
 }
 
 module.exports = texts;
